f2cf94bf4749eb691d0c939a2ae73f219e5790db
vpalcar <vidpalcar@outlook.com>
2023-09-11 14:32:25 +0200

    Prepering data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+const fs = require('fs');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const jsonString = await fs.readFileSync('./data.json', 'utf-8');
+    const data = JSON.parse(jsonString);
+    return data;
   }
 }
